328f7c31418e3ef1ef00bcde466c3340808be76b
Max45957 <46033233+Max45957@users.noreply.github.com>
2020-03-22 17:58:08 +0100

    Env var

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+
+require("dotenv").config(); //npm i dotenv
+const express = require("express"); //npm i express
+const bodyParser = require("body-parser"); //npm i body-parser
+const ejs = require("ejs"); //npm i ejs
+const mongoose = require("mongoose"); //npm i mongoose
+const encrypt = require("mongoose-encryption"); //npm i mongoose-encryption
+
+const app = express();
+
+console.log(process.env.SECRET);
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.route("/")
+.get(function(req, res) {
+  res.render("home");
+});
+
+app.route("/register")
+.get(function(req, res) {
+  res.render("register"); //ovo ejs "komanda"
+})
+.post(function(req, res) {
+  const newUser = new User({
+    //sve ispod je mongoose "komanda"
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.route("/login")
+.get(function(req, res) {
+  res.render("login");
+})
+.post(function(req, res) {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      if (foundUser) {
+        if (foundUser.password == password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+mongoose.connect("mongodb://localhost:27017/UserDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+//vazno je da je ova linija prije modela
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]}); //password se odnosi na password dokument u userSchema
+
+const User = new mongoose.model("User", userSchema);
+
+app.listen(3000, function() {
+  console.log("Server started.");
+});
